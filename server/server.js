3bdfd185ae9edd3ea26f707b2e2038e2e0bf7caa
Wentai Liu <liuwentai0629@gmail.com>
2023-12-16 02:13:44 -0800

    Add structure

--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@ import express from "express";
 import 'dotenv/config';
 import connectDB from './db/connectDB.js';
 import UserRouter from './routers/userRouter.js';
+import AuthRouter from './routers/authRouter.js'
 
 const PORT = 3000;
 const app = express();
 
 connectDB();
+
+app.use(express.json());
+
+app.use('/api/auth', AuthRouter);
 
 app.use('/api/user', UserRouter);
 
---
+++ server/src/auth/router.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get('/login');
+
+router.post('/signup');
+
+export default router;
